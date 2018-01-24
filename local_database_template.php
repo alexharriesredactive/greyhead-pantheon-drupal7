6ffbdea38f1143fed81937525e8c784ec89674e0
Alex Harries <alex@greyhead.co.uk>
2018-01-24 16:36:53 +0000

    Added local database template.
    Signed-off-by: Alex Harries <alex@greyhead.co.uk>

---
+++ local_database_template.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * local_database_template.php
+ *
+ * If your site uses the sites/default directory - e.g. on Pantheon - then
+ * you just need to paste the database connection array into this file.
+ *
+ * E.g.:
+
+$databases = array (
+  'default' =>
+  array (
+    'default' =>
+    array (
+      'database' => 'greyhead_d7_pantheon',
+      'username' => 'root',
+      'password' => 'password',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
+
+ */
