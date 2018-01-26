e023437fabeeac99c7d4d9acaac53f7ee59da43c
Alex Harries <alex@greyhead.co.uk>
2018-01-26 14:09:37 +0000

    Add tag filter to customisable content listings.
    Signed-off-by: Alex Harries <alex@greyhead.co.uk>

--- web/sites/all/modules/features/paragraph_page/templates/node--paragraph-page.tpl.php
+++ web/sites/all/modules/features/paragraph_page/templates/node--paragraph-page.tpl.php
@@ -82,12 +82,14 @@
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php if (!($view_mode == 'full')): ?>
-    <?php print render($title_prefix); ?>
-    <h2<?php print $title_attributes; ?>>
-      <a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-    <?php print render($title_suffix); ?>
-  <?php endif ?>
+  <?php if ($view_mode != 'full'): ?>
+    <?php if (!empty($node->title)): ?>
+      <?php print render($title_prefix); ?>
+      <h2<?php print $title_attributes; ?>>
+        <a href="<?php print $node_url; ?>"><?php print $node->title; ?></a></h2>
+      <?php print render($title_suffix); ?>
+    <?php endif; ?>
+  <?php endif; ?>
 
   <div class="content"<?php print $content_attributes; ?>>
     <?php if ($display_submitted): ?>
@@ -99,10 +101,10 @@
     <?php endif; ?>
 
     <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
+    // We hide the comments and links now so that we can render them later.
+    hide($content['comments']);
+    hide($content['links']);
+    print render($content);
     ?>
   </div>
 
