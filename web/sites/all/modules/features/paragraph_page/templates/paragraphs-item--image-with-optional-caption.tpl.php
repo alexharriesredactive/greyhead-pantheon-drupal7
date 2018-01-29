15022a8313e885f6af0cab09aa6ea246b3725495
Alex Harries <alex@greyhead.co.uk>
2018-01-29 14:16:52 +0000

    Add "image with optional caption" paragraph.
    Signed-off-by: Alex Harries <alex@greyhead.co.uk>

---
+++ web/sites/all/modules/features/paragraph_page/templates/paragraphs-item--image-with-optional-caption.tpl.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file
+ * paragraphs-item--image-with-optional-caption.tpl.php
+ */
+?>
+
+<div class="paragraphs-item paragraphs-item--image-with-optional-caption">
+  <div class="row">
+    <div class="container">
+      <div class="image-and-caption">
+        <div class="image">
+          <?php print render($content['field_parapg_image_w_caption_img']); ?>
+        </div>
+
+        <?php if ($field_parapg_image_w_caption_cap = render($content['field_parapg_image_w_caption_cap'])): ?>
+          <div class="caption">
+            <?php print $field_parapg_image_w_caption_cap; ?>
+          </div>
+        <?php endif ?>
+      </div>
+    </div>
+  </div>
+</div>
